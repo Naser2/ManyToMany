4cbbf671c4a917169d6efeb8f5a35fbe48853bc8
github username <github email address>
2018-09-21 11:29:05 -0400

    add Favorite

---
+++ favorite.rb
@@ -0,0 +1,20 @@
+class Favorite
+
+  #Class variable
+  @@all = []
+
+  attr_reader :user, :tweet
+
+    def initialize(tweet, user)
+      @tweet = tweet
+      @user = user
+      @all << self
+    end
+
+    #Class Method
+    def self.all
+      @all 
+    end
+
+
+end
